3a4f1650e6dd327a5b367874a9e03dd63f2c2f2f
Mustajab Ikram <mustajab.22.1@mountblue.tech>
2023-03-01 19:30:11 +0530

    Initial repo setup completed

---
+++ index.js
@@ -0,0 +1,14 @@
+// Import packages
+const express = require('express');
+const home = require('./routes/home');
+
+// Middleware
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use('/api', home);
+
+// connection
+const port = process.env.PORT || 8080;
+app.lis(port, () => console.log(`Server Running On PORT: ${port}`));

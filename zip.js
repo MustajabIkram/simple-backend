82eb9faa15a16e5e44a68f58e2bc277750a65fb8
Mustajab Ikram <mustajab.22.1@mountblue.tech>
2023-03-02 13:32:01 +0530

    configuring res.zip two levels up

--- zip.js
+++ zip.js
@@ -2,7 +2,7 @@ const AdmZip = require('adm-zip');
 const path = require('path');
 module.exports = async function createZipArchive() {
   const zip = new AdmZip();
-  const outputFile = 'res.zip';
+  const outputFile = '../../tmp/res.zip';
   zip.addLocalFolder(path.join(__dirname, '../../tmp/'));
   const data = zip.toBuffer();
   zip.writeZip(outputFile);

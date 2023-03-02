dfdaececa98af2a6860df9497833efcc0a8589fe
Mustajab Ikram <mustajab.22.1@mountblue.tech>
2023-03-02 13:46:59 +0530

    downloading file.docx

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use('/api/check', check);
 // Routes
 app.get('/api/download', function (req, res) {
   try {
-    const folderPath = __dirname + '../../tmp/output-1.docx';
+    const folderPath = __dirname + '../../tmp/file.docx';
     res.download(folderPath, () => {
       // fs.unlinkSync(path.join(__dirname, '../../tmp/file.docx'));
       // fs.unlinkSync(path.join(__dirname, '../../tmp/data.xlsx'));
